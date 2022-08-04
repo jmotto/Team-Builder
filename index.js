f3c80303b3d2482f7cb1e4696960bd3b2f27af82
Jamie Otto <jmotto31@gmail.com>
2022-08-03 18:20:23 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Create an empty array list to store employee objects
+const employeeArray = () => {}
+//  Gather manager data
+
+    // inquirer.prompt()
+
+    // THEN Build a Manager object
+
+// Gather Engineer data
+
+    // inquirer.prompt()
+    
+    // THEN Build a Engineer object
+
+// Gather Intern data
+
+    // inquirer.prompt()
+
+    // THEN Build a Intern object
+
+
+// Ask for which team member they want to add or "are they done"
+
+    // inquirer.prompt()
+
+    // THEN deciding which function to call 
+
+// Generate the HTML and write it to a file
+
+
