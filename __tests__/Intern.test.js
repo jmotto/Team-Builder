d98e055643eaf12979b14b0a978c2690521aeb9d
Jamie Otto <jmotto31@gmail.com>
2022-08-10 14:12:02 -0700

    added intern test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,36 @@
+const Intern = require('../lib/Intern');
+
+describe("Intern", () => {
+    const name = "Joe Intern";
+    const id = 246810;
+    const email = "joe@intern.com";
+    const school = "University of Washington";
+    const role = "Intern";
+
+    const testIntern = new Intern(name, id, email, school, role);
+
+    it("should return Intern name", () => {
+        
+        expect(testIntern.getName()).toBe(name);
+    })
+
+    it("should return Intern id", () => {
+        
+        expect(testIntern.getId()).toBe(id);
+    })
+
+    it("should return Intern email", () => {
+        
+        expect(testIntern.getEmail()).toBe(email);
+    })
+
+    it("should return Intern school", () => {
+        
+        expect(testIntern.getSchool()).toBe(school);
+    })
+
+    it("should return Intern role", () => {
+        
+        expect(testIntern.getRole()).toBe(role);
+    })
+});
