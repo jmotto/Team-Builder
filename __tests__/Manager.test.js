337323c9bb1c31188e64a472f28ada19a047821b
Jamie Otto <jmotto31@gmail.com>
2022-08-10 14:16:59 -0700

    added manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,37 @@
+const Manager = require('../lib/Manager');
 
+describe("Manager", () => {
+    const name = "Jamie Otto";
+    const id = 13579;
+    const email = "jamie@manager.com";
+    const officeNumber = 108;
+    const role = "Manager";
+
+    const testManager = new Manager(name, id, email, officeNumber, role);
+
+    it("should return Manager name", () => {
+        
+        expect(testManager.getName()).toBe(name);
+    })
+
+    it("should return Manager id", () => {
+        
+        expect(testManager.getId()).toBe(id);
+    })
+
+    it("should return Manager email", () => {
+        
+        expect(testManager.getEmail()).toBe(email);
+    })
+
+    it("should return Manager Office Number", () => {
+        
+        expect(testManager.getOfficeNumber()).toBe(officeNumber);
+    })
+
+    it("should return Manager role", () => {
+        
+        expect(testManager.getRole()).toBe(role);
+    })
+});
 
